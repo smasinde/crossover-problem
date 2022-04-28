4993714cb4855fd94e74dff8576c8dabd78278f8
smasinde <smasinde@gmail.com>
2022-04-29 01:39:32 +0300

    major changes

--- index.js
+++ index.js
@@ -1,103 +1,107 @@
-const axios = require('axios').default;
-const { AsyncResource } = require('async_hooks');
-const EventEmitter = require('events');
-const path = require('path');
+const axios = require("axios").default;
+const { AsyncResource } = require("async_hooks");
+const EventEmitter = require("events");
+const path = require("path");
 
 const {
-    Worker, isMainThread, parentPort, workerData, threadId
-} = require('worker_threads');
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
+  threadId,
+} = require("worker_threads");
 
-const url = process.argv[process.argv.findIndex(value => value.includes("https://"))];
+const url = new URL(
+  process.argv[
+    process.argv.findIndex(
+      (value) => value.includes("https://") || value.includes("www.")
+    )
+  ]
+);
 
-axios.get(url, {
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0',
-    }
-}).then(response => {
-    if (response.status === 200) {
-        const pool = new WorkerPool(100, url);
-        pool.setMaxListeners(0);
-        console.log('Pool created..')
-        pool.runTask(url, (e) => console.log(e))
-    }
-})
 class WorkPoolCrawlInfo extends AsyncResource {
-    constructor(callback) {
-        super("WorkPoolCrawlInfo");
-        this.callback = callback;
-    }
+  constructor(callback) {
+    super("WorkPoolCrawlInfo");
+    this.callback = callback;
+  }
 
-    done(err, result) {
-        this.runInAsyncScope(this.callback, null, err, result);
-        this.emitDestroy;
-    }
+  done(err, result) {
+    this.runInAsyncScope(this.callback, null, err, result);
+    this.emitDestroy;
+  }
 }
+
 const kTaskInfo = Symbol("kTaskInfo");
 const kWorkerFreedEvent = Symbol("kWorkerFreedEvent");
 
 class WorkerPool extends EventEmitter {
-    constructor(numberOfThreads, baseUrl) {
-        super();
-        this.numberOfThreads = numberOfThreads;
-        this.baseUrl = baseUrl;
-        this.workers = [];
-        this.freeWorkers = [];
-        this.scanned = new Set();
+  constructor(numberOfThreads) {
+    super();
+    this.numberOfThreads = numberOfThreads;
+    this.workers = [];
+    this.freeWorkers = [];
+    this.scanned = new Set();
 
-        for (let i = 0; i < numberOfThreads; i++) {
-            this.addNewWorker();
-        }
+    for (let i = 0; i < numberOfThreads; i++) {
+      this.addNewWorker();
     }
+    console.log(`Pool created with ${numberOfThreads} workers(s).`);
+  }
 
-    addNewWorker() {
-        const worker = new Worker(path.resolve(__dirname, 'worker.js'));
-        worker.setMaxListeners(0);
+  addNewWorker() {
+    const worker = new Worker(path.resolve(__dirname, "worker.js"));
+    worker.setMaxListeners(0);
 
-        worker.on('message', (result) => {
-            // this.close();
-            console.log("kFreedEvent");
-
-            this.freeWorkers.push(worker);
-            this.emit(kWorkerFreedEvent);
-            if (result.size !== 0) result.forEach((link) => { this.runTask(link, e => console.log(e)) });
+    worker.on("message", (result) => {
+      this.freeWorkers.push(worker);
+      this.emit(kWorkerFreedEvent);
+      if (result.size !== 0) {
+        result.forEach((link) => {
+          if (!this.scanned.has(link))
+            this.runTask(link, (e) => console.log(e));
         });
-        worker.on('error', (err) => {
-            console.log(err);
-            console.log("freeWorker");
-            this.freeWorkers.push(worker);
-            this.emit(kWorkerFreedEvent);
-        })
+      }
+    });
+    worker.on("error", (err) => {
+      this.freeWorkers.push(worker);
+      this.emit(kWorkerFreedEvent);
+    });
 
-        this.workers.push(worker);
-        this.freeWorkers.push(worker);
-        this.emit(kWorkerFreedEvent);
+    this.workers.push(worker);
+    this.freeWorkers.push(worker);
+    this.emit(kWorkerFreedEvent);
+  }
+
+  runTask(task, callback) {
+    if (this.scanned.has(task)) {
+      return;
     }
 
-    runTask(task, callback) {
-        if (this.scanned.has(task)) {
-            return;
-        }
+    if (this.freeWorkers.length === 0) {
+      this.once(kWorkerFreedEvent, function () {
         this.scanned.add(task);
-        if (this.freeWorkers.length === 0) {
-            this.once(kWorkerFreedEvent, () => {
-                this.runTask(task, callback)
-            });
-            return;
-        }
-        console.log('acquiring worker');
-        const worker = this.freeWorkers.pop();
-
-        worker[kTaskInfo] = new WorkPoolCrawlInfo(callback);
-
-        worker.onmessage = function (e) {
-            console.log('Message received from worker: , ', e.data)
-        }
-
-        worker.postMessage(task);
-        console.log('Message posted to worker');
+        this.runTask(task, callback);
+      });
+      return;
     }
 
-    close() {
-        for (const worker of this.workers) worker.terminate();
-    }
+    console.log("ADD TASK:::", task);
+    this.scanned.add(task);
+
+    const worker = this.freeWorkers.pop();
+
+    worker[kTaskInfo] = new WorkPoolCrawlInfo(callback);
+
+    worker.postMessage({ task: task, scanned: this.scanned, baseUrl: url.href });
+  }
+
+  close() {
+    for (const worker of this.workers) worker.terminate();
+  }
 }
+
+const pool = new WorkerPool(50);
+pool.setMaxListeners(0);
+
+pool.runTask(url.href);
+// pool.runTask(url, () => console.log("**run task callback**"));
--- worker.js
+++ worker.js
@@ -1,41 +1,46 @@
-const { parentPort, threadId } = require('worker_threads');
-const axios = require('axios').default;
-const { parse } = require('node-html-parser');
+const { parentPort, threadId } = require("worker_threads");
+const axios = require("axios").default;
+const { parse } = require("node-html-parser");
 
-parentPort.on('message', async (task) => {
-    console.log('Message received from main script, threadNumber:', threadId);
-
-    await axios.get(task, {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0',
-        }
-    }).then(response => {
-        let linkSet = new Set();
-        console.log("doing work..");
-        if (response.status === 200){
-            const html = response.data;
-            const root = parse(html);
-            const links = root.getElementsByTagName("a");
-
-            for (let link of links) {
-                let href = link.getAttribute("href");
-                console.log("HREF::::::::",href);
-                if (String(href).startsWith("/")) {
-                    linkSet.add(task.concat("", href))
-                } else if (String(href).includes(task) && String(href) !== process.argv[process.argv.findIndex(value => value.includes("https://"))]) {
-                    linkSet.add(href);
-                }
-            }
-            parentPort.postMessage(linkSet);
-        }
-    }).catch(e => {
-        console.log("Worker error:",e.response.status);
-        parentPort.postMessage(new Set());
+parentPort.on("message", async (data) => {
+  await axios
+    .get(data.task, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0",
+      },
     })
-    
-})
+    .then((response) => {
+      let linkSet = new Set();
+
+      if (response.status === 200) {
+        const html = response.data;
+        const root = parse(html);
+        const links = root.getElementsByTagName("a");
+
+        for (let link of links) {
+          let href = link.getAttribute("href");
+
+          if (String(href).startsWith("/#") || String(href).startsWith("/")) {
+            const relativeLink = data.baseUrl.concat("", href.slice(1));
+            linkSet.add(relativeLink);
+          } else if (
+            String(href).includes(data.task) &&
+            String(href) !== data.baseUrl
+          ) {
+            linkSet.add(href);
+          }
+        }
+        parentPort.postMessage(linkSet);
+      }
+    })
+    .catch((e) => {
+      console.log("Worker error:", e);
+      parentPort.postMessage(new Set());
+    });
+});
 // parentPort.on('message', (task) => {
 //     console.log(`running task on thread: ${threadId} with link: ${task}`);
-    
+
 //     parentPort.postMessage(task);
 // })
